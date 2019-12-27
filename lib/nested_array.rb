38de6beff806ae9236b40f31e581d34aeebbf17c
K e n V <kenneth_valdivia@yahoo.com>
2019-12-27 22:27:33 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,13 +36,17 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  organic_produce_1 = ["Strawberries"] #row 1
+  organic_produce_1 = ["Strawberries"] # row 1
 organic_produce_2 = ["Potatoes"] # row 2
 organic_produce_3 = ["Grapes"] # row 3
-organic_produce_4 = ["Avocadoes"] #row 4
+organic_produce_4 = ["Avocadoes"] # row 4
 organic_produce_5 = ["Asparagus"] # row 5
 
-
+conventional_produce_1 = ["Grapefruit"] # column 1
+conventional_produce_2 = ["Pineapple"] # column 2
+conventional_produce_3 = ["Oranges"] # column 3
+conventional_produce_4 = ["Watermelon"] # column 4
+conventional_produce_5 = ["Eggplant"] # column 5
 end
 
 def matrix_update(matrix, row, column, new_value)
