bdd15412a2f47f95b9eecc44480c13df0595d07b
K e n V <kenneth_valdivia@yahoo.com>
2019-12-27 22:29:01 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,21 +17,36 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  organic_produce_1 = ["Strawberries"]
+organic_produce_2 = ["Potatoes"]
+organic_produce_3 = ["Grapes"]
+organic_produce_4 = ["Avocadoes"]
+organic_produce_5 = ["Asparagus"]
+
+conventional_produce_1 = ["Grapefruit"]
+conventional_produce_2 = ["Pineapple"]
+conventional_produce_3 = ["Oranges"]
+conventional_produce_4 = ["Watermelon"]
+conventional_produce_5 = ["Eggplant"]
 end
 
 def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
-  # uses the arrays of conventional and organic produce as before.
-  # However, this time, sort each internal array alphabetically by the first character
+  ORGANIC_PRODUCE == CONVENTIONAL_PRODUCE
+  ["Asparagus"],["Avocadoes"],["Eggplant"]["Grapefruit"],["Grapes"],["Oranges"],["Pineapple"],["Potatoes"],["Strawberries"],["Watermelon"]
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  organic_produce_1 = ["Strawberries"] # row 1
+organic_produce_2 = ["Potatoes"] # row 2
+organic_produce_3 = ["Grapes"] # row 3
+organic_produce_4 = ["Avocadoes"] # row 4
+organic_produce_5 = ["Asparagus"] # row 5
+
+conventional_produce_1 = ["Grapefruit"] # column 1
+conventional_produce_2 = ["Pineapple"] # column 2
+conventional_produce_3 = ["Oranges"] # column 3
+conventional_produce_4 = ["Watermelon"] # column 4
+conventional_produce_5 = ["Eggplant"] # column 5
 end
 
 def matrix_update(matrix, row, column, new_value)
